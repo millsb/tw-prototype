312dc8d312956a80556fe152ed401cde99217a3a
Bryan Mills <bryan@millz.net>
2011-05-03 21:35:37 -0400

    added vagrant file and recipes

--- Gemfile
+++ Gemfile
@@ -1,8 +1,11 @@
+source 'http://rubygems.org'
+source 'http://gems.github.com'
+
+gem 'thin'
 gem 'sinatra'
 gem 'haml'
 gem 'sass'
 gem 'compass'
-source 'http://gems.github.com'
 gem 'html5-boilerplate'
 
 group :development do
--- app.rb
+++ app.rb
@@ -1,18 +1,18 @@
-# This makes sure the bundled gems are in our $LOAD_PATH
-require File.expand_path(File.join(File.dirname(__FILE__), 'vendor', 'gems', 'environment'))
+require 'rubygems'
+require 'bundler'
 
 # This actually requires the bundled gems
-Bundler.require_env
+Bundler.require
 
 configure do
-  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config.rb'))
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config/config.rb'))
   set :haml, :format => :html5
   set :sass, Compass.sass_engine_options
 end
 
 get '/css/:name.css' do
   content_type 'text/css', :charset => 'utf-8'
-  sass(:"sass/#{params[:name]}", Compass.sass_engine_options)
+  scss(:"sass/#{params[:name]}", Compass.sass_engine_options)
 end
 
 get '/' do
